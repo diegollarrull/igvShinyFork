9cc1b49197b99c87c655568620de4f5a1f6f3929
Diego Llarrull <diego.llarrull@indear.com>
2020-11-18 10:36:44 -0300

    1.0.2 con posible workaround de issue en popup

--- inst/htmlwidgets/igvShiny.js
+++ inst/htmlwidgets/igvShiny.js
@@ -45,7 +45,7 @@ HTMLWidgets.widget({
                    Shiny.setInputValue("igv-trackClick", x, {priority: "event"})
                        // for use outside of the ShinyModule idiom
                    Shiny.setInputValue("trackClick", x, {priority: "event"})
-                   return false; // undefined causes follow on display of standard popup
+                   return true; // undefined causes follow on display of standard popup
                    }); // on
                 }); // then: promise fulflled
           },
